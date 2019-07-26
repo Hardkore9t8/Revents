b0dc209ea8f26cb463963e4d29a2ce878c4bff8e
Hardkore9t8 <nash3799@eagle.fgcu.edu>
2019-07-26 14:11:06 -0400

    End of section 8

--- src/features/event/EventList/EventList.jsx
+++ src/features/event/EventList/EventList.jsx
@@ -1,16 +1,20 @@
-import React, { Component, Fragment } from "react";
-import EventListItem from "./EventListItem";
+import React, { Component, Fragment } from 'react';
+import EventListItem from './EventListItem';
 
 class EventList extends Component {
   render() {
-    const {events, selectEvent, deleteEvent} = this.props;
+    const { events, deleteEvent } = this.props;
     return (
       <Fragment>
         {events.map(event => (
-          <EventListItem key={event.id} event={event} selectEvent= {selectEvent} deleteEvent={deleteEvent}/>
+          <EventListItem
+            key={event.id}
+            event={event}
+            deleteEvent={deleteEvent}
+          />
         ))}
       </Fragment>
-    )
+    );
   }
 }
-export default EventList
+export default EventList;
